19f9122705534e2819637b69ac0d8bb87db37e19
GabrielCarreiro <63800998+GabrielCarreiro@users.noreply.github.com>
2020-09-24 08:47:34 -0300

    Criação dos controles

--- src/pages/funcionarios/index.js
+++ src/pages/funcionarios/index.js
@@ -23,7 +23,7 @@ const Funcionarios = () => {
             console.log("loadFuncionario", error)
         }
     };
-    ;
+    
     useEffect(() => {
         loadFuncionarios();
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Funcionarios from '../pages/funcionarios';
-import Produtos from '../pages/produto'
-import Home from '../pages/home'
+import Produtos from '../pages/produto';
+import Home from '../pages/home';
+import ControleProduto from '../pages/controle/controleProduto';
 
 const Routes = ()=>(
     <Switch>
         <Route path="/" component={Home} exact/>
         <Route path="/funcionario" component={Funcionarios}/>
         <Route path="/produto" component={Produtos}/>
+        <Route path="/controle/produto" component={ControleProduto}/>
     </Switch>
 )
 
